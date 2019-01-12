37bb75bdfe061bab3b833f1f2dc11b1f35e3e17e
Max Dymond <cmeister2@gmail.com>
2019-01-12 00:28:43 +0000

    Added the ability to download images to the cache.

--- tests/test_scryfall_cache.py
+++ tests/test_scryfall_cache.py
@@ -3,13 +3,57 @@
 
 """Tests for `scryfall_cache` package."""
 
+import os
+import pytest
 import scryfall_cache
 
 
-def test_query_mtgo_id():
-    """Test querying for an MTGO ID."""
-    sc = scryfall_cache.ScryfallCache(application="scryfall_tests")
-    card = sc.card_from_mtgo_id(12345)
+# Have to define the scryfall cache at the session scope - there can be only
+# one otherwise Pony gets a bit upset.
+@pytest.fixture(scope="session")
+def scrycache():
+    """
+    Create a test session scope ScryfallCache.
+
+    Returns:
+        scryfall_cache.ScryfallCache: cache object.
+
+    """
+    return scryfall_cache.ScryfallCache(application="scryfall_tests")
+
+
+def test_query_mtgo_id(scrycache):
+    """
+    Test querying for an MTGO ID.
+
+    Args:
+        scrycache: The cache under test
+
+    """
+    card = scrycache.get_card(mtgo_id=12345)
 
     # Verify that card 12345 is 6875ce99-badd-44da-8e5d-509600efa1d0
-    assert card["id"] == "6875ce99-badd-44da-8e5d-509600efa1d0"
+    assert card.get_id() == "6875ce99-badd-44da-8e5d-509600efa1d0"
+
+
+def test_card_images(scrycache):
+    """Test card images.
+
+    Args:
+        scrycache: The cache under test
+
+    """
+    card = scrycache.get_card(12345)
+    path = card.get_image_path("png")
+    print(path)
+
+    # Verify that the path is as we expect
+    cache_path = os.path.join("png", "6875ce99-badd-44da-8e5d-509600efa1d0.png")
+    assert path.endswith(cache_path)
+
+    # Now try an art_crop.
+    crop_path = card.get_image_path("art_crop")
+    print(crop_path)
+
+    cache_path = os.path.join("art_crop", "6875ce99-badd-44da-8e5d-509600efa1d0.jpg")
+    assert crop_path.endswith(cache_path)
